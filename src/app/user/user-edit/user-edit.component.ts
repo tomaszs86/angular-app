022f6aba509a5acfd9fc64489fcf1dfd062327f4
edward zareba <tomaszstolarczyk86@gmail.com>
2017-05-27 21:41:50 +0200

    User edit

--- src/app/user/user-edit/user-edit.component.ts
+++ src/app/user/user-edit/user-edit.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import { User } from "app/user/user";
+import { UserService } from "app/user/user-service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-user-edit',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+  public user:any = {};
+  public id: string;
+  public errorMessage: string;
+
+  constructor(public userService: UserService, private route:ActivatedRoute, private router: Router ) { }
 
   ngOnInit() {
+
+    this.route.params.subscribe(params => {
+      this.id = this.route.snapshot.params['id'];
+
+      if (this.id) {
+        this.userService.getUser(this.id)
+          .subscribe(user => {
+            this.user = user;
+          });
+      }
+    });
   }
+
+public update(user: User){
+
+    this.userService.update(this.user)
+            .subscribe(
+              () => this.onSaveComplete(),
+              (error: any) => this.errorMessage = <any>error
+        );
+  }
+
+public onSaveComplete(): void {
+    this.router.navigate(['/users']);
+}
+
+public cancel(){
+    this.router.navigate(['/users']);
+} 
 
 }
