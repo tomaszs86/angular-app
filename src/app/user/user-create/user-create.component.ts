0372dbf8cf63af7de823f5a9dd73bea17f5fccad
edward zareba <tomaszstolarczyk86@gmail.com>
2017-05-27 21:39:42 +0200

    User create

--- src/app/user/user-create/user-create.component.ts
+++ src/app/user/user-create/user-create.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from "app/user/user-service";
+import { Router } from "@angular/router";
+import { User } from "app/user/user";
 
 @Component({
   selector: 'app-user-create',
   templateUrl: './user-create.component.html',
   styleUrls: ['./user-create.component.css']
 })
-export class UserCreateComponent implements OnInit {
+export class UserCreateComponent {
 
-  constructor() { }
+    id: number;
+    username: string;
+    isActive: boolean;
+    created: string;
+    user: User;
+    errorMessage: string;
 
-  ngOnInit() {
-  }
+    constructor( private userService: UserService, private router: Router  ){}
+
+    createUser(){
+        console.log("createUser");
+        this.user = new User ( 
+            this.id,
+            this.username, 
+            this.isActive, 
+            this.created
+        );
+
+        this.userService.create(this.user)
+            .subscribe(
+              () => this.onSaveComplete(),
+              (error: any) => this.errorMessage = <any>error
+        );
+    }  
+
+    onSaveComplete(): void {
+        this.router.navigate(['/users']);
+    }
 
 }
