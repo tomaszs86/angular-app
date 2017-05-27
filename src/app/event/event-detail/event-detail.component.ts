571f99c685bcba91387bed92c9f94fd68b7c63ad
edward zareba <tomaszstolarczyk86@gmail.com>
2017-05-27 22:47:29 +0200

    Event details

--- src/app/event/event-detail/event-detail.component.ts
+++ src/app/event/event-detail/event-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { EventService } from "app/event/event-service";
+import { Event } from "app/event/event";
+import { Session } from "app/session/session";
 
 @Component({
   selector: 'app-event-detail',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventDetailComponent implements OnInit {
 
-  constructor() { }
+  event:Event
+  addMode: boolean
+
+  filterBy: string = 'all';
+  sortBy: string = 'votes';
+
+  constructor(private eventService: EventService, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.data.forEach((data) => {
+      this.event = data['event'];
+      this.addMode = false;
+    })
+  }
+
+  addSession() {
+    this.addMode = true
+  }
+
+  saveNewSession(session:Session) {
+    const nextId =  Math.max.apply(null, this.event.sessions.map(s => s.id));
+    session.id = nextId + 1
+    this.event.sessions.push(session);
+    this.eventService.saveEvent(this.event).subscribe();
+    this.addMode = false
+  }
+
+  cancelAddSession() {
+    this.addMode = false
   }
 
 }
